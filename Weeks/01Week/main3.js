351c13c6c7339a60c2becb699505b2ffb5ba6250
Parmjeet <iamparmjeetmishra@gmail.com>
2024-06-03 20:09:14 +0530

    Folder rearrange

--- Weeks/01Week/main3.js
+++ Weeks/01Week/main3.js
@@ -12,16 +12,51 @@
 // const value1 = calcArithmetic(2, 2, 'sum')
 // console.log(value1, type)
 
-function calcArithmetic(a, b, type) {
-   let result = a + b
-   if (type == 'sum') {
-      return a + b
-   }
 
-   if (type == 'minus') {
-      return b - a
-   }
+// 2nd Way
+
+
+// function calcArithmetic(a, b, type) {
+//    if (type == 'sum') {
+//       return sum(a,b)
+//    }
+
+//    if (type == 'minus') {
+//       return minus(a,b)
+//    }
+// }
+
+// function sum(a, b) {
+//    return a + b
+// }
+
+// function minus(a, b) {
+//    return a - b
+// }
+
+// const value1 = calcArithmetic(2, 3, 'sum')
+// console.log(value1)
+
+
+
+// 3rd Way - Sort of Callback
+
+
+function calcArithmetic(a, b, typeFn) {
+   const ans = typeFn(a, b)
+   return ans
 }
 
-const value1 = calcArithmetic(2, 2, 'sum')
-console.log(value1, value1.type)
+function sum(a, b) {
+   return a + b
+}
+
+function minus(a, b) {
+   return a - b
+}
+
+const value1 = calcArithmetic(2, 3, sum)
+console.log(value1)
+
+
+//

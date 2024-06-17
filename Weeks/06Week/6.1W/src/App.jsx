74824f37b4b67f5fd392e3000bb07827b03aadbe
Parmjeet <iamparmjeetmishra@gmail.com>
2024-06-17 23:35:40 +0530

    Cohort Week 6.1 init

--- Weeks/06Week/6.1W/src/App.jsx
+++ Weeks/06Week/6.1W/src/App.jsx
@@ -4,9 +4,13 @@ export default function App() {
 
   const [ title, setTitle] = useState('my name is Parm')
 
+  const updateTitle = () => {
+    setTitle
+  }
+
   return (
     <div>
-      <button>Update the title</button>
+      <button onClick={updateTitle}>Update the title</button>
       <h2>{ title }</h2>
     </div>
   )

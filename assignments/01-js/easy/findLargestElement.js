82644ec8055af7b4599bdd29b8d14302a3fcb161
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-06 23:09:46 +0530

    Assignments-01js-Easy-> findLargest Element

--- assignments/01-js/easy/findLargestElement.js
+++ assignments/01-js/easy/findLargestElement.js
@@ -6,7 +6,16 @@
 */
 
 function findLargestElement(numbers) {
-    
+   let biggestEl = 0
+   for (let i = 0; i < numbers.length; i++) {
+      if (biggestEl < numbers[i]) {
+         biggestEl = numbers[i]
+      }
+   }
+   console.log(biggestEl)
+   return biggestEl
 }
+
+// findLargestElement([1,2,3,4, 99])
 
 module.exports = findLargestElement;

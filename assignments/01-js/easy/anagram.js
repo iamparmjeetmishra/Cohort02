50f9126dffcd0cb85144f128888c7a5cf7ae2352
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-06 23:42:24 +0530

    Assignments-01js-Easy->Anagram Finished not tested with jest

--- assignments/01-js/easy/anagram.js
+++ assignments/01-js/easy/anagram.js
@@ -4,8 +4,36 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
+// let word = 'fat'
 
+// let revWord = word.split('').sort()
+// console.log(revWord)
+
+// const array1 = [1, 30, 4, 21, 100000];
+// array1.sort();
+// console.log(array1);
+
+function isAnagram(str1, str2) {
+  let w1 = str1.length
+  let w2 = str2.length
+
+  if (w1 !== w2) {
+    // console.log('here 2')
+    return false
+  }
+
+  if (str1.split('').sort().join() !== str1) {
+    // console.log(true)
+    return false
+  } 
+
+  return true
+
+  
 }
+
+let check = isAnagram('tit', 'tit')
+
+console.log(check)
 
 module.exports = isAnagram;

b95420502400ee1c7c6d1192f39886eb5594b819
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-07 23:34:21 +0530

    expenditure analysis idea for solving que

--- assignments/01-js/easy/expenditure-analysis.js
+++ assignments/01-js/easy/expenditure-analysis.js
@@ -13,7 +13,39 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
+let arrs = [
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 10,
+		category: 'Food',
+		itemName: 'Pizza',
+  },
+  {
+		id: 2,
+		timestamp: 1656076800000,
+		price: 20,
+		category: 'Park',
+		itemName: 'Ride',
+  },
+  {
+		id: 1,
+		timestamp: 1656076800000,
+		price: 16,
+		category: 'Food',
+		itemName: 'Pizza2',
+	}
+]
+
+
+
+
 function calculateTotalSpentByCategory(transactions) {
+	// let category = arrs.map((arr) => arr.category)
+	let category = arrs.reduce((arr, cv) => 
+		arr.category
+	)
+  
   return [];
 }
 

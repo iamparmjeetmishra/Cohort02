f6c3474cac617384d155375a23948dc8c1655c65
Parmjeet <iamparmjeetmishra@gmail.com>
2024-07-20 23:47:36 +0530

    Weeks->W3->fetch v1

---
+++ Weeks/Week3/W3.1-Fetch/01fetch.js
@@ -0,0 +1,27 @@
+function getAnimalDAta() {
+   let data = fetch('https://fakerapi.it/api/v1/persons')
+   console.log(data)
+   let res = data.then((res) => {
+      return res.json()
+   })
+   console.log(res)
+   let finalData = res.then((fData) => {
+      console.log(fData)
+   })
+   // console.log(finalData)
+}
+
+
+
+getAnimalDAta()
+function getPersonsData() {
+   fetch('https://fakerapi.it/api/v1/persons')
+      .then((res) => {
+         res.json()
+            .then((fData) => {
+            console.log(fData)
+         })
+      })
+}
+
+// getPersonsData()

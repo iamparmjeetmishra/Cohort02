e5c5ac3b7daa5d64db3c5faad98607129137b38c
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-04 22:54:43 +0530

    Cohort->Quick test js file

--- quickTest.js
+++ quickTest.js
@@ -1,16 +1,33 @@
-const ALL_USERS = [
-   {
-      username: "johnwick@gmail.com",
-      password: "123",
-      name: 'John wick'
-   },
-   {
-      username: 'raman@gmail.com',
-      password: '1234',
-      name: 'Raman Singh'
-   },
-]
+let arrLike = {
+   0: 'a',
+   1: 'b',
+   2: 'c',
+   length: 3
+}
 
-let user = ALL_USERS.map(user => user.username && user.password ? console.log('hi') : 'null')
+console.log(typeof arrLike)
 
-console.log(user)
+
+
+
+
+
+
+
+
+// const ALL_USERS = [
+//    {
+//       username: "johnwick@gmail.com",
+//       password: "123",
+//       name: 'John wick'
+//    },
+//    {
+//       username: 'raman@gmail.com',
+//       password: '1234',
+//       name: 'Raman Singh'
+//    },
+// ]
+
+// let user = ALL_USERS.map(user => user.username && user.password ? console.log('hi') : 'null')
+
+// console.log(user)

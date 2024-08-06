6d7a4c1c3531a88a383be39c353861d3041a89f7
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-06 23:16:40 +0530

    Cohort->static files

--- quickTest.js
+++ quickTest.js
@@ -1,33 +1,4 @@
-let arrLike = {
-   0: 'a',
-   1: 'b',
-   2: 'c',
-   length: 3
-}
+let string = 'john'
 
-console.log(typeof arrLike)
-
-
-
-
-
-
-
-
-
-// const ALL_USERS = [
-//    {
-//       username: "johnwick@gmail.com",
-//       password: "123",
-//       name: 'John wick'
-//    },
-//    {
-//       username: 'raman@gmail.com',
-//       password: '1234',
-//       name: 'Raman Singh'
-//    },
-// ]
-
-// let user = ALL_USERS.map(user => user.username && user.password ? console.log('hi') : 'null')
-
-// console.log(user)
+let firstword = string.split('')[0]
+console.log(firstword)

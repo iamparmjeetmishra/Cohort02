c25a1f09a2886d362f7a11a2f5f63c7a8739fcc4
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-06 23:15:23 +0530

    Cohort->W5.1-FE-Todos(all todos)

---
+++ Weeks/Week5/W5.1/frontend/src/components/Todos.jsx
@@ -0,0 +1,13 @@
+
+
+export default function Todos({todos}) {
+   return <>
+      {todos.map((index, todo) => (
+         <div key={todo.id} className='border-2 border-sky-400 p-4 rounded'>
+            <h1 className='text-md font-semibold'>{ todo.title }</h1>
+            <h2 className='text-sm font-medium'>{todo.description}</h2>
+            <button className='text-sm bg-slate-300 py-1 px-1 rounded hover:bg-black hover:text-white mt-2'>{todo.completed === true ? 'Completed' : 'Mark as completed'}</button>
+         </div>
+      ))}
+   </>
+}

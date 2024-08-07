95bf2353e865a8db24b202d5e5229107c2a436c7
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-07 23:04:21 +0530

    Cohort->W7.1->static files

---
+++ Weeks/Week7/W7.1/Context/src/App.jsx
@@ -0,0 +1,25 @@
+import React, { Suspense } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Landing from './components/Landing';
+import Header from './components/Header';
+// import Dashboard from "./components/Dashboard";
+const Dashboard = React.lazy(() => import("./components/Dashboard"))
+const Prop = React.lazy(() => import('./components/Prop'))
+const Context = React.lazy(() => import('./components/Context'))
+
+export default function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/dashboard' element={<Suspense fallback={'loading...'}><Dashboard /></Suspense>} />
+        <Route path='/' element={<Landing />} />
+          <Route path='/prop' element={<Suspense fallback={'loading...'}><Prop /></Suspense>} />
+          <Route path='/context' element={<Suspense fallback={'loading...'}><Context /></Suspense>} />
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}

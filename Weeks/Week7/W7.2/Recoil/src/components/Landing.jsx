afecaac9c8b54415233d675a4621b6dc74f31e99
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-09 23:01:26 +0530

    Cohort->W7.2 Recoil Basic Landing page

---
+++ Weeks/Week7/W7.2/Recoil/src/components/Landing.jsx
@@ -0,0 +1,14 @@
+
+export default function Landing() {
+
+
+   
+
+   return (
+      <div className='flex flex-col items-center'>
+         <h2 className='text-2xl font-semibold py-4'>Todo App with backend</h2>
+         <div className='flex flex-col items-center gap-6'>            
+         </div>
+      </div>
+   )
+}

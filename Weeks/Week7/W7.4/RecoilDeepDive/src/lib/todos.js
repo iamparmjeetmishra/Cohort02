c5e7d2977f7b96a752e6e04e67207a541e44188b
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-12 23:26:26 +0530

    Cohort->W7.4->Recoip Deepdive-lib todos

---
+++ Weeks/Week7/W7.4/RecoilDeepDive/src/lib/todos.js
@@ -0,0 +1,12 @@
+export const TODOS = [
+   {
+      id: 1,
+      title: 'Go to gym',
+      description: 'Hit the gym for 5 days'
+   },
+   {
+      id: 2,
+      title: 'Fresh Food',
+      description: 'Eat Protein'
+   }
+]

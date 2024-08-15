8accd74f63609f55fd02f6b75ef46dca5414e6da
Parmjeet <iamparmjeetmishra@gmail.com>
2024-08-15 23:25:01 +0530

    Cohort->W8.2-FE-Landing page

---
+++ Weeks/Week8/W8.2/Paytm/src/components/Landing.jsx
@@ -0,0 +1,15 @@
+
+export default function Landing() {
+
+
+   
+
+   return (
+      <div className='flex flex-col items-center'>
+         <h2 className='text-2xl font-semibold py-4'>Paytm with BE</h2>
+         <div className='flex flex-col items-center gap-6'>
+            
+         </div>
+      </div>
+   )
+}

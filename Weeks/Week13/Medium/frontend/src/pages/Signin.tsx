4da55f033eaaef66d797f1d0bc9dd9fd473ae3b6
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-15 23:35:34 +0530

    signin page

--- Weeks/Week13/Medium/frontend/src/pages/Signin.tsx
+++ Weeks/Week13/Medium/frontend/src/pages/Signin.tsx
@@ -1,6 +1,15 @@
+import Quote from "@/components/Quote";
+import AuthForm from "@/components/AuthForm";
 
 export default function Signin() {
-  return (
-    <div>Signin</div>
-  )
+	return (
+		<main className="flex flex-col xl:flex-row min-h-screen">
+			<section className="h-auto w-full flex flex-col items-center justify-center m-auto ">
+				<AuthForm type="signin" />
+			</section>
+			<section className="h-auto w-full flex flex-col items-center justify-center bg-slate-200">
+				<Quote />
+			</section>
+		</main>
+	);
 }

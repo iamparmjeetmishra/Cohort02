7a0274e55d14a8a91bae470abed969ea20ea6db9
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-15 23:36:43 +0530

    quote component

---
+++ Weeks/Week13/Medium/frontend/src/components/Quote.tsx
@@ -0,0 +1,7 @@
+export default function Quote() {
+   return <main className="m-6 md:ms-14">
+      <h1 className="text-2xl md:text-3xl font-bold text-balance">"The customer support I received was exceptional. The support team went above and beyond to address my concerns"</h1>
+      <h2 className="text-xl font-semibold">Julies Winfield</h2>
+      <small className="opacity-40">CEO | Vector Group</small>
+   </main>;
+}

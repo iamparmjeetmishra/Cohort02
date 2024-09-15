5c1f407e0a735698c4109c97ab5ad29658b40b52
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-15 23:35:46 +0530

    signup page

--- Weeks/Week13/Medium/frontend/src/pages/Signup.tsx
+++ Weeks/Week13/Medium/frontend/src/pages/Signup.tsx
@@ -1,6 +1,15 @@
+import Quote from "@/components/Quote";
+import AuthForm from "@/components/AuthForm";
 
 export default function Signup() {
-  return (
-    <div>Signup</div>
-  )
+	return (
+		<main className="flex flex-col xl:flex-row min-h-screen">
+			<section className="h-auto w-full flex flex-col items-center justify-center m-auto ">
+				<AuthForm type="signup" />
+			</section>
+			<section className="h-auto w-full flex flex-col items-center justify-center bg-slate-200">
+				<Quote />
+			</section>
+		</main>
+	);
 }

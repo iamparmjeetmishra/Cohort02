1a35bdf8d2a42165b6cc880c5eea24f918ab279d
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-16 18:19:30 +0530

    zod updated

--- Weeks/Week13/Medium/common/src/index.ts
+++ Weeks/Week13/Medium/common/src/index.ts
@@ -1,24 +1,22 @@
 import {z} from 'zod'
 
 export const SignupInputProps = z.object({
-	name: z.string().optional(),
+	name: z.string(),
 	password: z.string().min(6, { message: 'Password must be 6 charactes or more' }),
-	username: z.string().email()
+	email: z.string().email()
 })
 
 export const SigninInputProps = z.object({
-   username: z.string().email(),
+   email: z.string().email(),
    password: z.string().min(6, {message: 'Password must be atleast 6 characters or more'})
 })
 
 export const CreateBlogInput = z.object({
    title: z.string(),
    content: z.string(),
-   id: z.string()
 })
 
 export const UpdateBlogInput = z.object({
-   id: z.string(),
    title: z.string().optional(),
    content: z.string().optional(),
 })

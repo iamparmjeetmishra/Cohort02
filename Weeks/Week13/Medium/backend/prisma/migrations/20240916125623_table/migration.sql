c762f5fbaba89946703a91f100397416a78f8fd3
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-17 23:52:34 +0530

    W13->BE->Prisma

---
+++ Weeks/Week13/Medium/backend/prisma/migrations/20240916125623_table/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - Added the required column `updateAt` to the `Post` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updateAt` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Made the column `name` on table `User` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updateAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updateAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "name" SET NOT NULL;

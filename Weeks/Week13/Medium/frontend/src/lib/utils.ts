4579b590db6ae63085a45e56fc21fc8803f35da3
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-17 23:54:36 +0530

    W13->FE->static files

--- Weeks/Week13/Medium/frontend/src/lib/utils.ts
+++ Weeks/Week13/Medium/frontend/src/lib/utils.ts
@@ -1,6 +1,30 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+	return twMerge(clsx(inputs));
+}
+
+export function avatarWord(input: string) {
+	return input.split("")[0].toUpperCase();
+}
+
+export function ToLocaleDate(input: Date) {
+	const date = new Date(input);
+
+	// format the date
+	const readbleDate = date.toLocaleString("en-US", {
+		year: "numeric",
+		month: "long",
+		day: "2-digit",
+		hour: "2-digit",
+		minute: "2-digit",
+		hour12: true,
+	});
+
+	return readbleDate;
+}
+
+export function CapitalizeFirstWord(input: string) {
+	return input.charAt(0).toUpperCase() + input.slice(1)
 }

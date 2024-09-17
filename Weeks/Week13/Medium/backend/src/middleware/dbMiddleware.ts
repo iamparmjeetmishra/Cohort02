70ebdffc9e8341d552917ecb07f2a9f415fb2008
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-17 23:53:03 +0530

    W13->BE->DB middleware for connection

--- Weeks/Week13/Medium/backend/src/middleware/dbMiddleware.ts
+++ Weeks/Week13/Medium/backend/src/middleware/dbMiddleware.ts
@@ -2,25 +2,31 @@ import { PrismaClient } from "@prisma/client";
 import { Pool, neon, neonConfig } from "@neondatabase/serverless";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import ws from "ws";
-import { env } from "hono/adapter";
 
-let prisma: PrismaClient | null = null
+let prisma: PrismaClient | null = null;
 
 export const getPrisma = async (connectionString: string) => {
-   console.log('prisma db')
-   try {
-      if (!prisma) {
-         neonConfig.webSocketConstructor = ws;
-         const pool = new Pool({ connectionString });
-         const adapter = new PrismaNeon(pool);
+	console.log("prisma db");
+	try {
+		if (!prisma) {
+			neonConfig.webSocketConstructor = ws;
+			const pool = new Pool({ connectionString });
+			const adapter = new PrismaNeon(pool);
 
-         prisma = new PrismaClient({ adapter });
+			prisma = new PrismaClient({ adapter });
 
-         await prisma.$connect();
-      }
-      return prisma
+			await prisma.$connect();
+		}
+		return prisma;
 	} catch (error) {
-      console.log("dbErr:", error);
-      throw error
+		console.log("dbErr:", error);
+		throw error;
+	}
+};
+
+export const closePrisma = async () => {
+	if (prisma) {
+		await prisma.$disconnect();
+		prisma = null;
 	}
 };

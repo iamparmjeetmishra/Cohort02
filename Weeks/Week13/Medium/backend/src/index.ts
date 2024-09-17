6fa6946b4d96bc2dccd8888ffcd003ec6f026559
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-17 23:53:58 +0530

    W13->BE->Entry file

--- Weeks/Week13/Medium/backend/src/index.ts
+++ Weeks/Week13/Medium/backend/src/index.ts
@@ -1,8 +1,7 @@
-import { Hono } from 'hono'
-import { verify } from 'hono/jwt'
-import { userRouter } from './routes/user';
-import { blogRouter } from './routes/blog';
-
+import { Hono } from "hono";
+import { userRouter } from "./routes/user";
+import { blogRouter } from "./routes/blog";
+import { cors } from "hono/cors";
 
 const app = new Hono<{
 	Bindings: {
@@ -11,15 +10,13 @@ const app = new Hono<{
 	};
 }>();
 
+app.use("/*", cors());
 
-app.route('/api/v1/user', userRouter)
-app.route('/api/v1/blog', blogRouter)
+app.route("/api/v1/user", userRouter);
+app.route("/api/v1/blog", blogRouter);
 
+app.get("/", (c) => {
+	return c.text("Hello Hono!");
+});
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-
-
-
-export default app
+export default app;
